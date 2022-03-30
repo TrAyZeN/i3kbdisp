1bc60d12b346419ce4441f7b4eb3e3e3ead0477b
TrAyZeN <1810leo@gmail.com>
2022-03-30 20:02:25 +0200

    feat: Handle '--release'

--- src/parser.js
+++ src/parser.js
@@ -7,13 +7,13 @@ function parseI3Config(content) {
 
     const keymap = [];
     for (let i = 0; i < lines.length;i++) {
-        const match = lines[i].match(/^bindsym\s+([^\s]+(\s*\+\s*[^\s]+)*)\s+([^\n]+)$/);
+        const match = lines[i].match(/^bindsym\s+(--release\s+)?([^\s]+(\s*\+\s*[^\s]+)*)\s+([^\n]+)$/);
 
         if (!match) {
             continue;
         }
 
-        keymap.push([match[1].split('+').map(b => b.trim()), match[3]]);
+        keymap.push([match[2].split('+').map(b => b.trim()), match[4]]);
     }
 
     return keymap;
